af93922fbf861c3e38501fb168541cd61503c96e
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-11-17 15:44:17 -0300

    .

--- provateste.py
+++ provateste.py
@@ -1,3 +1,7 @@
+import re
+
+
+
 testeA = [
      "Como posso atualizar meu cartão de crédito?", "Preciso mudar a forma de pagamento, o que fazer?", "Quero atualizar minhas informações de pagamento", "Método de pagamento desatualizado, como proceder para atualizar?"
 ]
@@ -9,14 +13,28 @@ input_text = ""
 
 
 
-
+# $(como)|$(Como) & (atualizar) & (crédito) 
+# (forma) (pagamento) *(Preciso)
+# (pagamento) (atualizar)
+# $(Método) (pagamento) ^(atualizar)
+# (mudar) (forma de pagamento) 
 
 def solicite(a):
-    intencoes = {}
-    acoes = {}
+    intencoes = {
+
+    }
+    acoes = {
+        'forma de pagamento':"",
+        'atualizar pagamento':"",
+        'metodo de pagamento':"",
+        'mudar forma de pagamento':"",
+    }
     input_text = a
     if input_text == "sair":
         return False
+    matches = re.findall(r'[+-]?\d+(?:\.\d+)?', input_text)
+    print(matches)
+    
     return True
 
 
